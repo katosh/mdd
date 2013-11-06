02339503e9fdfd1394420b3d2494efdcc31cb9fc
katosh <dominik.otto@gmail.com>
2013-11-06 22:32:36 +0100

    Aufgabe 1b

---
+++ aufgabe1b.m
@@ -0,0 +1,15 @@
+n=1000;
+x=0:1/n:1;
+
+for i=1:2
+
+	[e,y]=saufg(i*2,2,x);
+	figure(1+((i-1)*2))
+	plot(x,y(2,:))
+	title(['Histogramm fuer a = ',i*2,' S']);
+
+	figure(2+((i-1)*2))	
+	plot(x,y(3,:))
+	title(['Histogramm fuer a = ',i*2,' S^2']);
+	
+end
